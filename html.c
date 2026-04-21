bd42ad34f2e379037c9ce3775ef47d118ad4e24f
harivonyR* <harivonyratefiarison@gmail.com>
2026-04-21 21:23:18 +0300

    ADD CSS style

--- html.c
+++ html.c
@@ -28,8 +28,10 @@ void write_html_header() {
     write_html("    <meta charset=\"UTF-8\">\n");
     write_html("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
     write_html("    <title>Document</title>\n");
+    write_html("    <link rel=\"stylesheet\" type=\"text/css\" href=\"table.css\">\n");
     write_html("</head>\n");
     write_html("<body>\n");
+    write_html("<h1>CSV to HTML Table : INT student list with github</h1>\n");
 }
 
 void write_footer() {
